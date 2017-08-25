a9b43ee6c109b57a71313c9ab4dceab60df735e6
georgesdaniel <30408831+georgesdaniel@users.noreply.github.com>
2017-08-25 15:44:36 +0200

    TP Conflit LOCK et COMMIT

---
+++ Router.java
@@ -0,0 +1,108 @@
+package fr.codevallee.formation.tp;
+
+import static spark.Spark.get;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import fr.codevallee.formation.tp.modele.Demo;
+
+import freemarker.template.Configuration;
+import freemarker.template.Version;
+import spark.ModelAndView;
+import spark.servlet.SparkApplication;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Router implements SparkApplication {
+
+	public void init() {
+
+		final Logger logger = LoggerFactory.getLogger(Router.class);
+
+		
+		get("/exemple1", (request, response) -> {
+
+			logger.debug("start");
+
+			Map<String, Object> attributes = new HashMap<>();
+
+			// Exemple 1 (à déplacer dans une classe statique !):
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("formation");
+			EntityManager entityManager = entityManagerFactory.createEntityManager();
+			
+			// AJout d'une commune
+			Commune commune = new Commune();
+			commune.setNom("Clermont ferrand");
+			
+			entityManager.getTransaction().begin();
+			entityManager.persist(commune);
+			entityManager.getTransaction().commit();
+			
+			//Ajout d'un Maire
+			Maire maire = new Maire();
+			//maire.setNom ("zouba");
+					
+			TypedQuery<Maire> query = entityManager.createQuery("from Maire", Maire.class);
+			attributes.put("objets", query.getResultList());
+			
+			// Tp conflit avec commit
+						           
+			entityManager.getTransaction().begin();
+			Maire testmaire = entityManager.find(Maire.class, 46L) ;
+			testmaire.setNom("zouba");
+			//entityManager.persist(testmaire);
+			
+			entityManager.getTransaction().commit();
+			
+			
+			//Suppression d'un Maire
+			
+			//Maire supMaire = entityManager.find(Maire.class, 43L);
+			
+			//entityManager.getTransaction().begin();
+			//entityManager.remove(supMaire);
+			//entityManager.getTransaction().commit();
+			
+						
+			
+			// J'ajoute un métier :
+			//Demo metier = new Demo();
+			//metier.setNom("exemple1");
+			
+			//TypedQuery<Demo> query = entityManager.createQuery("from Demo", Demo.class);
+			//attributes.put("objets", query.getResultList());
+			
+			//entityManager.getTransaction().begin();
+			//entityManager.persist(metier);
+			//entityManager.getTransaction().commit();
+			entityManager.close();
+			
+			//TypedQuery<Maire> query2 = entityManager.createQuery ("from Maire", Maire.class);
+			//attributes.put("objets",query2.getResultList());
+
+			return new ModelAndView(attributes, "home.ftl");
+			
+		}, getFreeMarkerEngine());
+				
+
+	}
+
+	private FreeMarkerEngine getFreeMarkerEngine() {
+		FreeMarkerEngine engine = new FreeMarkerEngine();
+		Configuration configuration = new Configuration(new Version(2, 3, 23));
+		configuration.setTemplateUpdateDelayMilliseconds(Long.MAX_VALUE);
+		configuration.setClassForTemplateLoading(FreeMarkerEngine.class, "");
+		engine.setConfiguration(configuration);
+		return engine;
+	}
+
+}
+	
